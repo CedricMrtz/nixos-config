86b4d6c26932ff324dc4f5937464476d2439a85b
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-04-05 17:22:33 -0700

    hyprland config corrected

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,7 +1,6 @@
 { config, pkgs, inputs, system, ... }: 
 {
-  wayland.windowManager.hyprland = {
-    home.packages = with pkgs; [
+  home.packages = with pkgs; [
     kitty
     wofi
     waybar
@@ -11,6 +10,70 @@
     wl-clipboard
     grim
     slurp
-    ];
-  }
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${system}.hyprland;
+    settings = {
+      "$mod" = "SUPER";
+      monitor = ",preferred,auto,1";
+      exec-once = [
+        "waybar"
+        "hyprpaper"
+        "dunst"
+      ];
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(cba6f7ff)";
+        "col.inactive_border" = "rgba(6c7086ff)";
+        layout = "dwindle";
+      };
+      decoration = {
+        rounding = 10;
+        blur = {
+          enabled = true;
+          size = 8;
+          passes = 2;
+        };
+      };
+      animations.enabled = true;
+      input = {
+        kb_layout = "us";
+        follow_mouse = 1;
+        touchpad.natural_scroll = true;
+      };
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod, D, exec, wofi --show drun"
+        "$mod, Q, killactive"
+        "$mod, F, fullscreen"
+        "$mod, Space, togglefloating"
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
+        "$mod SHIFT, H, movewindow, l"
+        "$mod SHIFT, L, movewindow, r"
+        "$mod SHIFT, K, movewindow, u"
+        "$mod SHIFT, J, movewindow, d"
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+      ];
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
+  };
 }
