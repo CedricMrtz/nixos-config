ec360cd873c4e457711da46736efa2941120851c
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-04-21 13:51:30 -0700

    rmpc and brave added

--- home/cedric/home.nix
+++ home/cedric/home.nix
@@ -18,6 +18,7 @@
     ../../modules/lazyvim.nix
     # ../../modules/hyprland.nix
     ../../modules/niri.nix
+    ../../modules/ohmyzsh.nix
   ];
 
   programs.zen-browser.enable = true;
@@ -44,47 +45,9 @@
     };
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      gc = "sudo nix-collect-garbage -d";
-      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#desktop";
-    };
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "sudo" ];
-    };
-
-    # Source P10k theme and its instant prompt
-    initExtra = ''
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
-
-      export POWERLEVEL9K_CONFIG_FILE=~/nixos-config/dotfiles/p10k/.p10k.zsh
-      [[ -f ~/nixos-config/dotfiles/p10k/.p10k.zsh ]] && source ~/nixos-config/dotfiles/p10k/.p10k.zsh
-    '';
-  };
-  
-  programs.zsh.plugins = [
-    {
-      name = "powerlevel10k";
-      src = pkgs.zsh-powerlevel10k;
-      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    }
-  ];
-
-  xdg.desktopEntries = builtins.listToAttrs (map (app: {
-    name = app;
-    value = { name = app; noDisplay = true; exec = app; };
-  }) [ "xterm" "contacts" "audio player" ]);
-  
   home.packages = with pkgs; [
     obsidian
+    rmpc
+    brave
   ];
 }
