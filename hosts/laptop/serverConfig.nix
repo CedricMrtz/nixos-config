15b70878650198f1356be29f3657ad0b1bcf966b
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-08-26 11:59:21 -0700

    comment unused imports

--- hosts/laptop/serverConfig.nix
+++ hosts/laptop/serverConfig.nix
@@ -10,10 +10,10 @@
     home-manager.users.cedric = lib.mkForce (import ../../home/server/home.nix);
 
     virtualisation.oci-containers.backend = "podman";
-    virtualisation.oci-containers.containers = {
-      immich = { };
-      navidrome = { };
-    };
+    # virtualisation.oci-containers.containers = {
+    #   immich = { };
+    #   navidrome = { };
+    # };
     networking.firewall.allowedTCPPorts = [ 2283 4533 ];
   };
 }
