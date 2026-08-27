04f6cd1f3cbc630e6236fe1869c8890ba072e29c
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-08-26 22:57:24 -0700

    change bootloader

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -13,7 +13,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda"; # ajustar según el disco real
+  boot.loader.grub.device = "/dev/nvme0n1";
 
   users.users.cedric = {
     isNormalUser = true;
